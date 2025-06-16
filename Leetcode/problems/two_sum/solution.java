75eb2b9fb977a3c62d86caa77111c9314e910914
Rajdeep Debnath <debnathrajdeep30082003@gmail.com>
2025-06-16 13:19:31 +0000

    Sync LeetCode submission - Two Sum (java)

--- Leetcode/problems/two_sum/solution.java
+++ Leetcode/problems/two_sum/solution.java
@@ -1,28 +1,16 @@
-import java.util.Scanner;
 class Solution {
-    public int[] twoSum(int[] num, int target) {
-        int arr[]=new int[2];
-        for(int i=0;i<num.length-1;i++){
-            for (int j=i+1;j<num.length;j++){
-                if(num[i]+num[j]==target){
-                    arr[0]=i;
-                    arr[1]=j;
+    public int[] twoSum(int[] nums, int target) {
+        int low=0,high=nums.length-1,i;
+        HashSet<Integer> temp = new HashSet<>();
+        for(int a:nums)temp.add(a);
+        for(i=0;i<nums.length;i++){
+            if(temp.contains(target-nums[i])){
+                for(int j=i+1;j<nums.length;j++){
+                    if(nums[j]==(target-nums[i])) return new int[]{i,j};
                 }
-                
             }
         }
-    return arr;
-    }
+        return new int[]{-1,-1};
 
-    public static void main(String[] args){
-        Solution ob = new Solution();
-        Scanner sc = new Scanner(System.in);
-        int a[] = new int[1000];
-        for(int i=0;i<a.length;i++){
-            a[i]=sc.nextInt();
-        }
-        int n=sc.nextInt();
-        int sol[] = ob.twoSum(a,n);
-        System.out.println(sol);
     }
 }
