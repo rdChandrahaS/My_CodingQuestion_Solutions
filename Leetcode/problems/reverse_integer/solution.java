e5a00878236e83269ceceb64240d88bacc032cf7
Rajdeep Debnath <debnathrajdeep30082003@gmail.com>
2025-06-16 13:32:08 +0000

    Sync LeetCode submission - Reverse Integer (java)

--- Leetcode/problems/reverse_integer/solution.java
+++ Leetcode/problems/reverse_integer/solution.java
@@ -1,34 +1,13 @@
 class Solution {
     public int reverse(int x) {
-        int n,lastDigit,newNum1;
-        long newNum=0,limit = 2^31;
-        if(x<0)
-            n=x*(-1);
-        else
-            n=x;
-        while(n!=0){
-            lastDigit=n%10;
-            newNum=newNum*10+lastDigit;
-            n=n/10;
+        int sign = x<0?-1:1;
+        x=Math.abs(x);
+        long newnum=0,rem;
+        while(x>0){
+            rem=(long)(x%10);
+            newnum=newnum*10+rem;
+            x/=10;
         }
-
-        if ((newNum <= Integer.MAX_VALUE) && (newNum >= Integer.MIN_VALUE)) {
-            newNum1 = (int)newNum;
-            if(x<0)
-            return newNum1*(-1);
-            else if(x==0)
-            return 0;
-            else
-            return newNum1;
-        }
-        else
-        return 0;
-    }
-    public static void main(String[] args) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        int n = sc.nextInt();
-        Solution ob = new Solution();
-        int ans = ob.reverse(n);
-        System.out.println(ans);
+        return (newnum>Integer.MAX_VALUE || newnum<Integer.MIN_VALUE) ?0: (int)((sign==-1)?-1*newnum:newnum);
     }
 }
