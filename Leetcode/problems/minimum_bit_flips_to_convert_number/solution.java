657420e683f8db50c68b42352c64907e9ac779ef
Rajdeep Debnath <debnathrajdeep30082003@gmail.com>
2025-08-28 18:42:58 +0000

    Sync LeetCode submission - Minimum Bit Flips to Convert Number (java)

---
+++ Leetcode/problems/minimum_bit_flips_to_convert_number/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int num = start^goal;
+        int ans = 0;
+        while(num>0){
+            if(num%2==1){
+                ans++;
+            }
+            num/=2;
+        }
+        return ans;
+    }
+}
