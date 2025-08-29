bf13fb89b1fc839f85a5917910a2cf72e49ee0e8
Rajdeep Debnath <debnathrajdeep30082003@gmail.com>
2025-08-29 18:41:45 +0000

    Sync LeetCode submission - XOR Operation in an Array (java)

---
+++ Leetcode/problems/xor_operation_in_an_array/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int ans = start;
+        for(int i = start+2 ; i <= start + 2*(n-1) ; i+=2){
+            ans = ans^i;
+        }
+        return ans;
+    }
+}
