ef8369784c2a2e43e99ad7207c5243fde9d1727c
Rajdeep Debnath <debnathrajdeep30082003@gmail.com>
2025-11-08 14:46:14 +0000

    Sync LeetCode submission - Largest Number (java)

---
+++ Leetcode/problems/largest_number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] str = new String[nums.length];
+        for(int i=0 ;i<str.length;i++){
+            str[i]=String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(str,(a,b)->(b+a).compareTo(a+b));
+            
+        if(str[0].equals("0"))
+            return "0";
+        StringBuilder sb = new StringBuilder();
+        for(String s : str) sb.append(s);
+
+        return sb.toString();
+}
+}
